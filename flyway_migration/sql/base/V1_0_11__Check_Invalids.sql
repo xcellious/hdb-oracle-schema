64c481ac6dffb0e645042c22b0a33b79da673fe7
xcellious <38482887+xcellious@users.noreply.github.com>
2026-04-24 16:57:29 -0400

    remove unreleased flyway scripts from main

--- flyway_migration/sql/base/V1_0_11__Check_Invalids.sql
+++
@@ -1,5 +0,0 @@
--- Final check for invalid objects in HDBDBA and DECODES schemas
-SELECT owner, object_type, object_name 
-FROM dba_objects 
-WHERE status = 'INVALID' AND owner IN ('HDBDBA', 'DECODES')
-ORDER BY owner, object_type, object_name;
--- flyway_migration/sql/base/V1_0_12__Check_Errors.sql
+++
@@ -1,5 +0,0 @@
--- Check for compilation errors
-SELECT name, type, text 
-FROM dba_errors 
-WHERE owner IN ('HDBDBA', 'DECODES') 
-ORDER BY owner, name, type, line;
